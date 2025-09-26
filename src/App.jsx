d9469b748b756757ce82e5e2275178381b05dc1a
Dibbo <divyajitchakraborty@gmail.com>
2025-09-26 13:18:05 +0600

    Added Cards Placeholder

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-// import { useState } from "react";
+// import { use, useState } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Progress from "./components/Progress";
+import Tickets from "./components/Tickets";
 
 function App() {
   return (
     <>
       <Navbar></Navbar>
       <Progress></Progress>
+      <Tickets></Tickets>
       <Footer></Footer>
     </>
   );
---
+++ src/components/Tickets.jsx
@@ -0,0 +1,205 @@
+// import React, { use } from "react";
+import "./Tickets.css";
+
+// const TicketManagement () => {
+//   const jsonData = use(fetchTickets)
+// }
+
+// const fetchTickets = async () => {
+//   const res = await fetch("/assets/tickets.json");
+//   console.log(res);
+//   return res.json();
+// };
+
+const Tickets = () => {
+  // const fetchPromise = fetchTickets();
+
+  return (
+    <div id="tickets-container">
+      <div className="tickets-cards-parent">
+        <p className="tickets-title"></p>
+        <div className="tickets-gallery">
+          {/* *** */}
+
+          <div className="ticket-card">
+            <div className="tickets-1st">
+              <p className="ticket-title">ticket-title</p>
+              <div className="ticket-status"></div>
+            </div>
+            <p className="ticket-desc">ticket-desc</p>
+            <div className="ticket-3rd">
+              <div className="ticket-3rd-left">
+                <p className="ticket-id">ticket-id</p>
+                <p className="ticket-priority">prio</p>
+              </div>
+              <div className="ticket-3rd-right"></div>
+              <p className="ticket-username">username</p>
+              <p className="ticket-date">date</p>
+            </div>
+          </div>
+          <div className="ticket-card">
+            <div className="tickets-1st">
+              <p className="ticket-title">ticket-title</p>
+              <div className="ticket-status"></div>
+            </div>
+            <p className="ticket-desc">ticket-desc</p>
+            <div className="ticket-3rd">
+              <div className="ticket-3rd-left">
+                <p className="ticket-id">ticket-id</p>
+                <p className="ticket-priority">prio</p>
+              </div>
+              <div className="ticket-3rd-right"></div>
+              <p className="ticket-username">username</p>
+              <p className="ticket-date">date</p>
+            </div>
+          </div>
+          <div className="ticket-card">
+            <div className="tickets-1st">
+              <p className="ticket-title">ticket-title</p>
+              <div className="ticket-status"></div>
+            </div>
+            <p className="ticket-desc">ticket-desc</p>
+            <div className="ticket-3rd">
+              <div className="ticket-3rd-left">
+                <p className="ticket-id">ticket-id</p>
+                <p className="ticket-priority">prio</p>
+              </div>
+              <div className="ticket-3rd-right"></div>
+              <p className="ticket-username">username</p>
+              <p className="ticket-date">date</p>
+            </div>
+          </div>
+          <div className="ticket-card">
+            <div className="tickets-1st">
+              <p className="ticket-title">ticket-title</p>
+              <div className="ticket-status"></div>
+            </div>
+            <p className="ticket-desc">ticket-desc</p>
+            <div className="ticket-3rd">
+              <div className="ticket-3rd-left">
+                <p className="ticket-id">ticket-id</p>
+                <p className="ticket-priority">prio</p>
+              </div>
+              <div className="ticket-3rd-right"></div>
+              <p className="ticket-username">username</p>
+              <p className="ticket-date">date</p>
+            </div>
+          </div>
+          <div className="ticket-card">
+            <div className="tickets-1st">
+              <p className="ticket-title">ticket-title</p>
+              <div className="ticket-status"></div>
+            </div>
+            <p className="ticket-desc">ticket-desc</p>
+            <div className="ticket-3rd">
+              <div className="ticket-3rd-left">
+                <p className="ticket-id">ticket-id</p>
+                <p className="ticket-priority">prio</p>
+              </div>
+              <div className="ticket-3rd-right"></div>
+              <p className="ticket-username">username</p>
+              <p className="ticket-date">date</p>
+            </div>
+          </div>
+          <div className="ticket-card">
+            <div className="tickets-1st">
+              <p className="ticket-title">ticket-title</p>
+              <div className="ticket-status"></div>
+            </div>
+            <p className="ticket-desc">ticket-desc</p>
+            <div className="ticket-3rd">
+              <div className="ticket-3rd-left">
+                <p className="ticket-id">ticket-id</p>
+                <p className="ticket-priority">prio</p>
+              </div>
+              <div className="ticket-3rd-right"></div>
+              <p className="ticket-username">username</p>
+              <p className="ticket-date">date</p>
+            </div>
+          </div>
+          <div className="ticket-card">
+            <div className="tickets-1st">
+              <p className="ticket-title">ticket-title</p>
+              <div className="ticket-status"></div>
+            </div>
+            <p className="ticket-desc">ticket-desc</p>
+            <div className="ticket-3rd">
+              <div className="ticket-3rd-left">
+                <p className="ticket-id">ticket-id</p>
+                <p className="ticket-priority">prio</p>
+              </div>
+              <div className="ticket-3rd-right"></div>
+              <p className="ticket-username">username</p>
+              <p className="ticket-date">date</p>
+            </div>
+          </div>
+          <div className="ticket-card">
+            <div className="tickets-1st">
+              <p className="ticket-title">ticket-title</p>
+              <div className="ticket-status"></div>
+            </div>
+            <p className="ticket-desc">ticket-desc</p>
+            <div className="ticket-3rd">
+              <div className="ticket-3rd-left">
+                <p className="ticket-id">ticket-id</p>
+                <p className="ticket-priority">prio</p>
+              </div>
+              <div className="ticket-3rd-right"></div>
+              <p className="ticket-username">username</p>
+              <p className="ticket-date">date</p>
+            </div>
+          </div>
+          <div className="ticket-card">
+            <div className="tickets-1st">
+              <p className="ticket-title">ticket-title</p>
+              <div className="ticket-status"></div>
+            </div>
+            <p className="ticket-desc">ticket-desc</p>
+            <div className="ticket-3rd">
+              <div className="ticket-3rd-left">
+                <p className="ticket-id">ticket-id</p>
+                <p className="ticket-priority">prio</p>
+              </div>
+              <div className="ticket-3rd-right"></div>
+              <p className="ticket-username">username</p>
+              <p className="ticket-date">date</p>
+            </div>
+          </div>
+          <div className="ticket-card">
+            <div className="tickets-1st">
+              <p className="ticket-title">ticket-title</p>
+              <div className="ticket-status"></div>
+            </div>
+            <p className="ticket-desc">ticket-desc</p>
+            <div className="ticket-3rd">
+              <div className="ticket-3rd-left">
+                <p className="ticket-id">ticket-id</p>
+                <p className="ticket-priority">prio</p>
+              </div>
+              <div className="ticket-3rd-right"></div>
+              <p className="ticket-username">username</p>
+              <p className="ticket-date">date</p>
+            </div>
+          </div>
+
+          {/* *** */}
+        </div>
+      </div>
+
+      <div className="tickets-status">
+        <div className="tickets-status-right">
+          <p className="status-title">Task Status</p>
+          <p className="status-subtitle">
+            Select a ticket to add to Task Status
+          </p>
+        </div>
+        <div className="tickets-resolved">
+          <p className="resolved-title2">Resolved Task</p>
+          <p className="resolved-subtitle">No resolved tasks yet.</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Tickets;
